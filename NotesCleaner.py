43a4c686abdd71878b0cb72bbf2c4d22d0d7103d
drishyap23 <drishyarpandey@gmail.com>
2026-06-28 11:05:39 -0400

    commit19

--- NotesCleaner.py
+++ NotesCleaner.py
@@ -1,4 +1,13 @@
-f = open('class_notes.txt', 'r')
+file = open('class_notes.txt', 'r')
 n = int(input("Enter how many characters to preview: "))
-print(f.read(20))
-f.close()
+print(file.read(n))
+file.close()
+file = open('class_notes.txt', 'r')
+lines = file.readlines()
+print('Total lines: ', len(lines))
+for i in lines:
+    if i.startswith("Coding"):
+       print("skip", '->', i.strip())
+    else:
+       print("keep", '->', i.strip())
+file.close()
